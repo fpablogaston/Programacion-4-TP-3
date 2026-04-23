0025736a79f3c5f7bc7c755b6bb6f3d599829e31
David Resca <davidresca9@gmail.com>
2026-04-23 16:56:35 -0300

    ADD: Class DaoProducto + metodos agregarProducto y ejecutarSPAgregarProducto. Correcciones varias de indentacion

--- src/dao/DaoCategoria.java
+++ src/dao/DaoCategoria.java
@@ -22,26 +22,21 @@ public class DaoCategoria {
 		Connection cn = null; 
 		int filas = 0;
 		
-		try
-		{
-		cn = DriverManager.getConnection(host+dbName,user,pass);
-		Statement st = cn.createStatement();
-		filas = st.executeUpdate(query);
-		}
-		catch (Exception e)
-		{
-		e.printStackTrace();
+		try {
+			cn = DriverManager.getConnection(host+dbName,user,pass);
+			Statement st = cn.createStatement();
+			filas = st.executeUpdate(query);
+		} catch (Exception e){
+			e.printStackTrace();
 		}
 		return filas;
-		}
-	
+	}
 		
-		public Categoria obtenerCategoria(int id)
-		{
-			Categoria x = new Categoria();
-			Connection cn = null;
-			
-			try {
+	public Categoria obtenerCategoria(int id) {
+		Categoria x = new Categoria();
+		Connection cn = null;
+		
+		try {
 			cn = DriverManager.getConnection(host+dbName, user, pass);
 			Statement st = cn.createStatement();
 			String query = "Select * from Categorias where idCategoria = "+id;
@@ -49,99 +44,87 @@ public class DaoCategoria {
 			rs.next();
 			x.setIdCategoria(rs.getInt("IdCategoria"));
 			x.setNombre(rs.getString("nombre"));
-			}
-			catch(Exception e )
-			{
+		} catch(Exception e ) {
 			e.printStackTrace();
-			}
-			return x;
 		}
+		return x;
+	}
 		
-		public ArrayList<Categoria> obtenerTodasLasCategorias(){
+	public ArrayList<Categoria> obtenerTodasLasCategorias(){
+		
+		ArrayList<Categoria> lsCategoria = new ArrayList<Categoria>();
+		
+		Connection cn = null;
+		
+		try {
+			cn = DriverManager.getConnection(this.host + this.dbName, this.user, this.pass);
+			String query = "Select * from Categorias ";
+			Statement st = cn.createStatement();//ST ESTA PREPARADO PARA RECIBIR UNA CONSULTA
+			ResultSet rs = st.executeQuery(query);
 			
-			ArrayList<Categoria> lsCategoria = new ArrayList<Categoria>();
+			while(rs.next()) {
+				Categoria categoria = new Categoria();
+				categoria.setIdCategoria(rs.getInt(1));
+				categoria.setNombre(rs.getNString(2));
+				
+				lsCategoria.add(categoria);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return lsCategoria;	
+	}
+		
+	public int borrarCategoria(int idCategoria) {
+		String query = "Delete from Categorias where IdCategoria ="+idCategoria;
+		Connection cn = null;//DECLARAMOS UN OBJETO DE TIPO CONEXION
+		int filas = 0;
+		
+		try {//INTENTAMOS CARGAR LA CONEXION
+			//CN ES LA VARIABLE DE TIPO CONEXION
+			cn = DriverManager.getConnection(this.host + this.dbName, this.user, this.pass);//INSTANCIAMO CN HACIA UNA BASE DE DATOS
+			Statement st = cn.createStatement();//CREAMOS UNA CONSULTA PARA HACERLA SOBRE ESA BASE DE DATOS
+			filas = st.executeUpdate(query);
 			
-	        Connection cn = null;
-			
+		} catch (Exception e) {// y ACA SI NO PODEMOS CONCRETAR LA CONEXION
+			e.printStackTrace();
+		} finally {
 			try {
-				
-				
-				cn = DriverManager.getConnection(this.host + this.dbName, this.user, this.pass);
-				String query = "Select * from Categorias ";
-				Statement st = cn.createStatement();//ST ESTA PREPARADO PARA RECIBIR UNA CONSULTA
-				ResultSet rs = st.executeQuery(query);
-				while(rs.next()) {
-					Categoria categoria = new Categoria();
-					categoria.setIdCategoria(rs.getInt(1));
-					categoria.setNombre(rs.getNString(2));
-					
-					lsCategoria.add(categoria);
-				}
-				
-			}catch (Exception e) {
-				e.printStackTrace();
+				cn.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
 			}
-			
-			return lsCategoria;
-			
-			
 		}
 		
-		public int borrarCategoria(int idCategoria) {
-			String query = "Delete from Categorias where IdCategoria ="+idCategoria;
-			Connection cn = null;//DECLARAMOS UN OBJETO DE TIPO CONEXION
-			int filas = 0;
-			
-			
-			try {//INTENTAMOS CARGAR LA CONEXION
-				//CN ES LA VARIABLE DE TIPO CONEXION
-				
-				cn = DriverManager.getConnection(this.host + this.dbName, this.user, this.pass);//INSTANCIAMO CN HACIA UNA BASE DE DATOS
-				Statement st = cn.createStatement();//CREAMOS UNA CONSULTA PARA HACERLA SOBRE ESA BASE DE DATOS
-				filas = st.executeUpdate(query);
-				
-			} catch (Exception e) {// y ACA SI NO PODEMOS CONCRETAR LA CONEXION
-				e.printStackTrace();
-			} finally {
-				try {
-					cn.close();
-				} catch (Exception e2) {
-					e2.printStackTrace();
-				}
-			}
-			
-			return filas;
-		}
+		return filas;
+	}
 		
 		
-		public int ActualizarCategoria(Categoria categoria) {
-			String query = "UPDATE Categorias set  Nombre= ? where IdCategoria = ? ";
-			Connection cn = null;//DECLARAMOS UN OBJETO DE TIPO CONEXION
-			int filas = 0;
+	public int ActualizarCategoria(Categoria categoria) {
+		String query = "UPDATE Categorias set  Nombre= ? where IdCategoria = ? ";
+		Connection cn = null;//DECLARAMOS UN OBJETO DE TIPO CONEXION
+		int filas = 0;
+		
+		
+		try {//INTENTAMOS CARGAR LA CONEXION
+			//CN ES LA VARIABLE DE TIPO CONEXION
 			
+			cn = DriverManager.getConnection(this.host + this.dbName, this.user, this.pass);//INSTANCIAMO CN HACIA UNA BASE DE DATOS
+			PreparedStatement pst = cn.prepareStatement(query);//CREAMOS UNA CONSULTA PARA HACERLA SOBRE ESA BASE DE DATOS
+			pst.setString(1,categoria.getNombre());
+			pst.setInt(2, categoria.getIdCategoria());
+			filas = pst.executeUpdate();
 			
-			try {//INTENTAMOS CARGAR LA CONEXION
-				//CN ES LA VARIABLE DE TIPO CONEXION
-				
-				cn = DriverManager.getConnection(this.host + this.dbName, this.user, this.pass);//INSTANCIAMO CN HACIA UNA BASE DE DATOS
-				PreparedStatement pst = cn.prepareStatement(query);//CREAMOS UNA CONSULTA PARA HACERLA SOBRE ESA BASE DE DATOS
-				pst.setString(1,categoria.getNombre());
-				pst.setInt(2, categoria.getIdCategoria());
-				filas = pst.executeUpdate();
-				
-			} catch (Exception e) {
-				e.printStackTrace();
-			} finally {
-				try {
-					cn.close();
-				} catch (Exception e2) {
-					e2.printStackTrace();
-				}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				cn.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
 			}
-			
-			return filas;
 		}
-
-
-
+		
+		return filas;
+	}
 }
---
+++ src/dao/DaoProducto.java
@@ -0,0 +1,75 @@
+package dao;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import entidad.Producto;
+import java.sql.Statement;
+
+public class DaoProducto {
+
+	private String host ="jdbc:mysql://localhost:3306/";
+	private String user = "root";
+	private String pass = "root";
+	private String dbName = "bdinventario";
+	
+	public DaoProducto(){}
+
+
+	public int agregarProducto(Producto producto) {
+	    String query = "INSERT INTO Productos (Codigo, Nombre, Precio, Stock, IdCategoria) VALUES (?, ?, ?, ?, ?)";
+	    int filas = 0;
+	    Connection cn = null;
+	    PreparedStatement ps = null;
+
+	    try {
+	        cn = DriverManager.getConnection(host + dbName, user, pass);
+	        ps = cn.prepareStatement(query);
+	        ps.setString(1, producto.getCodigo());
+	        ps.setString(2, producto.getNombre());
+	        ps.setDouble(3, producto.getPrecio());
+	        ps.setInt(4, producto.getStock());
+	        ps.setInt(5, producto.getCategoria().getIdCategoria());
+	        filas = ps.executeUpdate();
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    } finally {
+	        try {
+	            if (ps != null) ps.close();
+	            if (cn != null) cn.close();
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	    return filas;
+	}
+
+
+	public void ejecutarSPAgregarProducto(Producto producto) {
+	    String query = "{CALL sp_AgregarProducto(?, ?, ?, ?, ?)}";
+	    Connection cn = null;
+	    CallableStatement cst = null;
+
+	    try {
+	        cn = DriverManager.getConnection(host + dbName, user, pass);
+	        cst = cn.prepareCall(query);
+	        cst.setString(1, producto.getCodigo());
+	        cst.setString(2, producto.getNombre());
+	        cst.setDouble(3, producto.getPrecio());
+	        cst.setInt(4, producto.getStock());
+	        cst.setInt(5, producto.getCategoria().getIdCategoria());
+	        cst.execute();
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    } finally {
+	        try {
+	            if (cst != null) cst.close();
+	            if (cn != null) cn.close();
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	}
+}
