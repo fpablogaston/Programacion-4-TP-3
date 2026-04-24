a96fb69a9069eaf15321f0020f449425fea183cd
Jonatan Chavez <jonichico400@gmail.com>
2026-04-24 19:24:25 -0300

    Agregue los setters y getters en la clase Categoria

--- src/entidad/Categoria.java
+++ src/entidad/Categoria.java
@@ -13,6 +13,23 @@ public class Categoria {
 		
 	}
 	
+	
+	public int getIdCategoria() {
+		return idCategoria;
+	}
+
+	public void setIdCategoria(int idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
 	@Override
 	public String toString() {
 		return "Categoria [ID=" + idCategoria + ", Nombre=" + nombre + "]";
